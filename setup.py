0caef6ccec6bc40f9b6351c54a31b595307d1060
Camille Avestruz <cavestru@umich.edu>
2020-06-05 16:31:41 -0400

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spa',
+      version='0.1',
+      description='Tests of spa measurements on 300 sims',
+      url='http://github.com/cavestruz/spa',
+      author='Camille Avestruz',
+      author_email='cavestru@umich.edu',
+      license='BSD3',
+      packages=['spa'],
+      zip_safe=False)
