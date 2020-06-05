f156f9befc6eb7f7935bbb05bfe18a0a067634b4
Andrew Gardner <andrewzg@umich.edu>
2020-06-05 17:08:00 -0400

    Test added a new function (visulaize_cluster)

--- spa/isocontours.py
+++ spa/isocontours.py
@@ -21,3 +21,22 @@ def get_pixels_in_contour(image_shape, contour) :
       Returns an nxm array of True and False'''
 
   return measure.grid_points_in_poly(image_shape, contour)
+
+def visualize_substructure(image_data, substructure_contour, axis,
+                          annotate=False, 
+                          plot_only_region=False) :
+  '''Visualize a cluster with imshow on an axis, options to draw contours and
+  to annotate.'''
+
+  if not plot_only_region: axis.imshow(image_data, norm=LogNorm())
+
+  axis.plot(substructure_contour[:,1],substructure_contour[:,0], lw=2., color='red') 
+  if plot_only_region : 
+    # 
+    _, region = get_region_properties(image_data, contour)
+    axis.imshow(region)
+
+  if annotate : 
+    axis.annotate(annotate, (0.5,0.9),fontsize='xx-large',
+                             color='white', xycoords='axes fraction') 
+
